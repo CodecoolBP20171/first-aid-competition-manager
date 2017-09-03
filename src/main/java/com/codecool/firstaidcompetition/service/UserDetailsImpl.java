c908eb3268f9fa67ae0f52bdcec9d3cd12558504
keligeri <kel.gergo@gmail.com>
2017-09-03 21:04:38 +0200

    Add UserDetailsService to use it in UserServiceImpl

---
+++ src/main/java/com/codecool/firstaidcompetition/service/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package com.codecool.firstaidcompetition.service;
+
+import com.codecool.firstaidcompetition.model.Role;
+import com.codecool.firstaidcompetition.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by keli on 2017.09.03..
+ */
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new HashSet<>();
+        Role userRole = user.getRole();
+        authorities.add( new SimpleGrantedAuthority(userRole.getRole()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true ;
+    }
+}
