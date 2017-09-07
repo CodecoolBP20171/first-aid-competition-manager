700b189cfd6ea853bc7b07247371518ffc27de9e
Balázs Máté <balazsm86@gmail.com>
2017-09-07 18:59:02 +0200

    Adding new station with drop-down competition list

--- src/main/java/com/codecool/firstaidcompetition/controller/HTTPController.java
+++ src/main/java/com/codecool/firstaidcompetition/controller/HTTPController.java
@@ -1,18 +1,21 @@
 package com.codecool.firstaidcompetition.controller;
 
+import com.codecool.firstaidcompetition.model.Competition;
+import com.codecool.firstaidcompetition.model.Station;
+import com.codecool.firstaidcompetition.model.User;
+import com.codecool.firstaidcompetition.repository.CompetitionRepository;
 import com.codecool.firstaidcompetition.repository.DBHandler;
-import com.codecool.firstaidcompetition.model.*;
 import com.codecool.firstaidcompetition.repository.StationRepository;
 import com.codecool.firstaidcompetition.repository.UserRepository;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.text.ParseException;
-import org.slf4j.Logger;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HTTPController {
@@ -25,12 +28,14 @@ public class HTTPController {
     private UserRepository userRepository;
     @Autowired
     private StationRepository stationRepository;
+    @Autowired
+    private CompetitionRepository competitionRepository;
 
     private boolean isDBUpdated = false;
 
     @RequestMapping(value = {"/", "/index"})
-    public String indexPage(){
-        if (!isDBUpdated){
+    public String indexPage() {
+        if (!isDBUpdated) {
             updateTable();
             isDBUpdated = true;
         }
@@ -38,13 +43,13 @@ public class HTTPController {
     }
 
     @GetMapping("/registration")
-    public String addUser(Model model){
+    public String addUser(Model model) {
         model.addAttribute("user", new User());
         return "registration_form";
     }
 
     @PostMapping("/registration")
-    public ModelAndView submitUser(@ModelAttribute User user){
+    public ModelAndView submitUser(@ModelAttribute User user) {
         userRepository.save(user);
 
         logger.info("Save USer to the db, " +
@@ -54,9 +59,8 @@ public class HTTPController {
         return new ModelAndView("redirect:/index");
     }
 
-
     @RequestMapping(value = {"/station"}, method = RequestMethod.GET)
-    public String getStations(Model model){
+    public String getStations(Model model) {
         Iterable<Station> stationList = stationRepository.findAll();
         model.addAttribute("listOfStations", stationList);
 
@@ -65,13 +69,15 @@ public class HTTPController {
     }
 
     @GetMapping(value = "station/add")
-    public String addStation(Model model){
+    public String addStation(Model model) {
+        Iterable<Competition> competitionList = competitionRepository.findAll();
+        model.addAttribute("listOfCompetitions", competitionList);
         model.addAttribute("station", new Station());
         return "station_form";
     }
 
     @PostMapping(value = "station/add")
-    public ModelAndView submitStation(@ModelAttribute Station station){
+    public ModelAndView submitStation(@ModelAttribute Station station) {
         // Query a user from the db (owner has to be redirect from the session)
         stationRepository.save(station);
         logger.info("Save station to the db, " +
@@ -81,11 +87,11 @@ public class HTTPController {
         return new ModelAndView("redirect:/station");
     }
 
-    public void updateTable(){
+    public void updateTable() {
         try {
             dbHandler.populateDB();
             logger.info("Table updated with dummy data");
-        } catch (ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
             logger.info("Can't update the table with dummy data");
         }
