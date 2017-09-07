905051a277cd40dbe82913de41b50982e0295939
keligeri <kel.gergo@gmail.com>
2017-09-07 19:36:39 +0200

    Define authentication with password hashing and user validation

--- src/main/java/com/codecool/firstaidcompetition/service/CustomAuthenticationProvider.java
+++ src/main/java/com/codecool/firstaidcompetition/service/CustomAuthenticationProvider.java
@@ -6,13 +6,10 @@ import com.codecool.firstaidcompetition.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -25,7 +22,7 @@ import java.util.*;
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
-    public BCryptPasswordEncoder bCryptPasswordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     private UserRepository userRepository;
 
@@ -33,7 +30,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
-        System.out.println(name + "  " + password);
         User user = authenticateUser(name, password);
         if (user != null) {
             List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
@@ -46,20 +42,12 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         } else {
             return null;
         }
-//        if (name.equals("admin") && password.equals("system")) {
-//            List<GrantedAuthority> grantedAuths = new ArrayList<>();
-//            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
-//            Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
-//            return auth;
-//        } else {
-//            return null;
-//        }
     }
 
     public User authenticateUser(String username, String password){
         User byUsername = userRepository.findByUserName(username);
         User byPassword = authenticateByPassword(password);
-        if (byUsername != null && byUsername.getId() == byPassword.getId()){
+        if (byUsername != null && byPassword != null && byUsername.getId() == byPassword.getId()){
             return byUsername;
         } else {
             return null;
