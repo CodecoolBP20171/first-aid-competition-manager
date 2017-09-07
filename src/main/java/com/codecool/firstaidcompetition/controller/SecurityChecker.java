fad38d1ea30892fa32df638306cc8684a2703370
keligeri <kel.gergo@gmail.com>
2017-09-07 19:33:24 +0200

    Add some dummy route to check access

--- src/main/java/com/codecool/firstaidcompetition/controller/SecurityChecker.java
+++ src/main/java/com/codecool/firstaidcompetition/controller/SecurityChecker.java
@@ -1,13 +1,9 @@
 package com.codecool.firstaidcompetition.controller;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by keli on 2017.09.01..
- */
 @RestController
 @Controller
 public class SecurityChecker {
