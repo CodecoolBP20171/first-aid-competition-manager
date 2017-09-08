8827bb5b9aeda5a3633b5566a8edc735f08a9cc2
keligeri <kel.gergo@gmail.com>
2017-09-08 17:45:31 +0200

    Validate userName and pass with ajax and prevent submit

--- src/main/resources/static/js/registrationValidation.js
+++ src/main/resources/static/js/registrationValidation.js
@@ -2,9 +2,31 @@
  * Created by keli on 2017.07.03..
  */
 $(document).ready(function(){
-    $("#submit_button").click(function() {
-        validatePassword();
+    $("#regForm").submit(function(evt) {    // check userName and pass before submit
+        var isValidUser = validateUserNameIsExists();
+        var isValidPass = validatePassword();
+        if (!isValidPass || !isValidUser){
+            evt.preventDefault();
+        }
     });
+
+    var validateUserNameIsExists = function() {
+        var userName = $('#user_name').val();
+        $.ajax({
+            method: "GET",
+            url: "/registration/" + userName,
+            success: function(data){
+                if (data === "true"){
+                    alert("A felhasználónév már használatban van");
+                    $("#user_name").val("");
+                    return true;
+                }
+            },
+            error: function(data){
+                alert("Something went wrong!");
+            }
+        })
+    };
 
     var validatePassword = function(){
         var $passwordOne = $('#password').val();
@@ -16,12 +38,15 @@ $(document).ready(function(){
                 alert("A jelszónak tartalmaznia kell legalább 1 kis- és nagybetűt, valamint egy számot is!");
                 $("#password").val("");
                 $("#password2").val("");
+                return false;
             }
         } else {
             alert("A két jelszó nem lehet eltérő!");
             $("#password").val("");
             $("#password2").val("");
+            return false;
         }
+        return true;
     }
 });
 
