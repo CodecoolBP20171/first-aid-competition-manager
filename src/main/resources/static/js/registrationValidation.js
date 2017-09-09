fc733cd90e616162ae0ba1312e15c46848e3d7df
keligeri <kel.gergo@gmail.com>
2017-09-09 11:32:07 +0200

    Extend authentication fundtions with colorChanger function

--- src/main/resources/static/js/registrationValidation.js
+++ src/main/resources/static/js/registrationValidation.js
@@ -2,58 +2,42 @@
  * Created by keli on 2017.07.03..
  */
 $(document).ready(function(){
-    $("#regForm").submit(function(event) {    // check userName and pass before submit
-        // https://api.jquery.com/submit/
-        var isValidUser = validateUserNameIsExists();
-        var isValidPass = validatePassword();
-        if (!isValidPass || !isValidUser){
-            event.preventDefault();
-            if (!isValidUser) {
-                console.log("user");
-                changeUserNameColor();
-            } else if (!isValidPass) {
-                console.log("pass");
-                changePasswordColor();
-            }
-        } else {
-            $('#regForm').submit();
-            alert("else ág");
-            // return;
-        }
 
+    $("#regForm").submit(function(event) {    // check userName and pass before submit
+        validateUserNameIsExists(function(data){
+            if (data === true){
+                event.preventDefault();
+                alert("A felhasználónév már használatban van");
+                $("#user_name").val("");
+                changeUserNameColor();
+            } else {
+                changeUserNameColorToDefault();
+            }
+        });
+
+        var isValidPass = validateUserPassword();
+        if (!isValidPass){
+            event.preventDefault();
+            changePasswordColor();
+        } else {
+            changePasswordColorToDefault();
+        }
     });
 
-    var changeUserNameColor = function (){
-        $("#user_name").parent(".input-group").attr("class", "input-group has-error");
-    };
-
-    var changePasswordColor = function(){
-        $("#password").parent(".input-group").attr("class", "input-group has-error");
-        $("#password2").parent(".input-group").attr("class", "input-group has-error");
-    };
-
-    var validateUserNameIsExists = function() {
+    var validateUserNameIsExists = function(callback) {
         var userName = $('#user_name').val();
-        $.ajax({
+        return $.ajax({
             method: "GET",
             url: "/registration/" + userName,
-            success: function(data){
-                if (data === "true"){
-                    alert("A felhasználónév már használatban van");
-                    $("#user_name").val("");
-                    return false;
-                } else {
-                    alert("A user átment" + data);
-                    return true;
-                }
-            },
-            error: function(data){
+            async: false,
+            success: callback,
+            error: function(){
                 alert("Something went wrong!");
             }
-        })
+        });
     };
 
-    var validatePassword = function(){
+    var validateUserPassword = function(){
         var $passwordOne = $('#password').val();
         var $passwordTwo = $('#password2').val();
         var reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/;
@@ -72,6 +56,26 @@ $(document).ready(function(){
             return false;
         }
         return true;
-    }
+    };
+
+    var changeUserNameColor = function (){
+        $("#user_name").parent(".input-group").attr("class", "input-group has-error");
+    };
+
+    var changeUserNameColorToDefault = function (){
+        $("#user_name").parent(".input-group").attr("class", "input-group");
+    };
+
+    var changePasswordColor = function(){
+        $("#password").parent(".input-group").attr("class", "input-group has-error");
+        $("#password2").parent(".input-group").attr("class", "input-group has-error");
+    };
+
+    var changePasswordColorToDefault = function(){
+        $("#password").parent(".input-group").attr("class", "input-group");
+        $("#password2").parent(".input-group").attr("class", "input-group");
+    };
+
+
 });
 
