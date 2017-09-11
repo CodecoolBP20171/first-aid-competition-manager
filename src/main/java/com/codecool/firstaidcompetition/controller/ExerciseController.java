8f61934903ace91c0d70ba5a1d30ba2da9811c46
keligeri <kel.gergo@gmail.com>
2017-09-11 11:55:16 +0200

    Add addExercise get, post method to create exercise

--- src/main/java/com/codecool/firstaidcompetition/controller/ExerciseController.java
+++ src/main/java/com/codecool/firstaidcompetition/controller/ExerciseController.java
@@ -2,15 +2,13 @@ package com.codecool.firstaidcompetition.controller;
 
 import com.codecool.firstaidcompetition.model.Exercise;
 import com.codecool.firstaidcompetition.repository.ExerciseRepository;
+import com.codecool.firstaidcompetition.repository.StationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.html.HTMLDocument;
-import java.util.List;
 
 /**
  * Created by keli on 2017.09.11..
@@ -19,10 +17,12 @@ import java.util.List;
 @RequestMapping("/exercise/")
 public class ExerciseController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HTTPController.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(ExerciseController.class);
 
     @Autowired
     private ExerciseRepository exerciseRepository;
+    @Autowired
+    private StationRepository stationRepository;
 
     @GetMapping("/table")
     private String listAllExercise(Model model){
@@ -33,7 +33,23 @@ public class ExerciseController {
 
     @GetMapping("/delete/{exerciseId}")
     private String deleteExercise(@PathVariable String exerciseId){
+        exerciseRepository.delete(Long.valueOf(exerciseId));
         logger.info("Deleted  exercise with id: {}", exerciseId);
+        return "exercises/exercise_table";
+    }
+
+    @GetMapping("/add")
+    private String addExercise(Model model){
+        model.addAttribute("exercise", new Exercise());
+        stationRepository.findAll();
+        model.addAttribute("listOfStations", stationRepository.findAll());
+
+        return "exercises/exercise_add";
+    }
+
+    @PostMapping("/add")
+    private String saveExercise(@ModelAttribute Exercise exercise, Model model){
+        exerciseRepository.save(exercise);
         return "exercises/exercise_table";
     }
 }
