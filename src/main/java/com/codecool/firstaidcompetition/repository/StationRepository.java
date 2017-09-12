f71c26c4ac415305a39d01ddd401774d104e0069
keligeri <kel.gergo@gmail.com>
2017-09-12 12:45:53 +0200

    Add findByExercise method to save exercise in proper way

--- src/main/java/com/codecool/firstaidcompetition/repository/StationRepository.java
+++ src/main/java/com/codecool/firstaidcompetition/repository/StationRepository.java
@@ -3,7 +3,11 @@ package com.codecool.firstaidcompetition.repository;
 import com.codecool.firstaidcompetition.model.Station;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by keli on 2017.07.06..
  */
-public interface StationRepository extends CrudRepository<Station, Long> {}
+public interface StationRepository extends CrudRepository<Station, Long> {
+    List<Station> findAllByExerciseIsNull();
+}
