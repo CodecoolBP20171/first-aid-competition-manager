9ef9e7460791208e24d70a41ba4d6d4a9823ae26
keligeri <kel.gergo@gmail.com>
2017-09-12 14:09:28 +0200

    Refactor db, override Exercise-Station relation from ManyToOne to OneToMany

--- src/main/java/com/codecool/firstaidcompetition/controller/ExerciseController.java
+++ src/main/java/com/codecool/firstaidcompetition/controller/ExerciseController.java
@@ -46,17 +46,19 @@ public class ExerciseController {
     @GetMapping("/add")
     private String addExercise(Model model){
         model.addAttribute("exercise", new Exercise());
-        model.addAttribute("listOfStations", stationRepository.findAllByExerciseIsNull());
+        model.addAttribute("listOfStations", stationRepository.findAll());
         return "exercises/exercise_add";
     }
 
     @PostMapping("/add")
     private ModelAndView saveExercise(@ModelAttribute Exercise exercise){
         exerciseRepository.save(exercise);
-        for (Station station : exercise.getStations()){
-            station.setExercise(exercise);
-            stationRepository.save(station);
-        }
+
+        //        for (Station station : exercise.getStation()){
+//            station.setExercise(exercise);
+//            stationRepository.save(station);
+//        }
+        System.out.println(exercise.getStation());
         return new ModelAndView("redirect:/index");
     }
 }
