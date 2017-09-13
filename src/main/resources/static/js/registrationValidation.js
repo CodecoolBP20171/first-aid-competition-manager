89af703ff867cbab0e324938d4d07a9185e39008
keligeri <kel.gergo@gmail.com>
2017-09-13 18:47:06 +0200

    Fix the bug on edit-delete modal window

--- src/main/resources/static/js/registrationValidation.js
+++ src/main/resources/static/js/registrationValidation.js
@@ -3,26 +3,15 @@
  */
 $(document).ready(function () {
 
-    $("#exercise_bin_icon").click(function(){
+    $(".exercise_bin_icon").click(function () {
         var exerciseId = $(this).attr("data-comp_id");
-        var deletedUrl = "/exercise/delete/" + exerciseId;
-        $("#deleteExercise").attr("href", deletedUrl);
+        updateDeleteExerciseModal(exerciseId);
     });
 
-    $("#exercise_edit_icon").click(function(){
+    $(".exercise_edit_icon").click(function () {
         var exerciseId = $(this).attr("data-comp_id");
-        $.ajax({
-            url: "/exercise/edit/" + exerciseId,
-            method: "GET",
-            success: function(data){
-                updateEditExerciseModal(data);
-            },
-            error: function(){
-                alert("bad")
-            }
-        })
+        updateEditExerciseModal(exerciseId);
     });
-
 
     $("#regForm").submit(function (event) {    // check userName and pass before submit
         validateUserNameIsExists(function (data) {
@@ -45,10 +34,24 @@ $(document).ready(function () {
         }
     });
 
-    var updateEditExerciseModal = function(data){
-        $("#exerciseName").val(data["name"]);
-        $("#exerciseDesc").val(data["description"]);
-        $("#exerciseId").val(data["id"]);
+    var updateDeleteExerciseModal = function (exerciseId) {
+        var deletedUrl = "/exercise/delete/" + exerciseId;
+        $("#deleteExercise").attr("href", deletedUrl);
+    };
+
+    var updateEditExerciseModal = function (exerciseId) {
+        $.ajax({
+            url: "/exercise/edit/" + exerciseId,
+            method: "GET",
+            success: function (data) {
+                $("#exerciseName").val(data["name"]);
+                $("#exerciseDesc").val(data["description"]);
+                $("#exerciseId").val(data["id"]);
+            },
+            error: function () {
+                alert("Something went wrong!")
+            }
+        })
     };
 
     var validateUserNameIsExists = function (callback) {
