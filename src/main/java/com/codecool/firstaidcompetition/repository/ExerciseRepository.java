9b48d2b9bfb0232d0cc17a6f2834074d5e0aa71d
keligeri <kel.gergo@gmail.com>
2017-09-20 16:50:55 +0200

    add findByCompetitionId and findByStationId

--- src/main/java/com/codecool/firstaidcompetition/repository/ExerciseRepository.java
+++ src/main/java/com/codecool/firstaidcompetition/repository/ExerciseRepository.java
@@ -3,5 +3,7 @@ package com.codecool.firstaidcompetition.repository;
 import com.codecool.firstaidcompetition.model.Exercise;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ExerciseRepository extends CrudRepository<Exercise, Long> {}
+public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
+    Iterable<Exercise> findByStationId(Long stationId);
+}
 
