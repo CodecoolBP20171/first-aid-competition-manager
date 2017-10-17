51e1d5f8c0c8b0e6606fd352fe05ed50ce63b1d5
keligeri <kel.gergo@gmail.com>
2017-10-17 16:15:31 +0200

    Add custom exception, use it in the StationController

--- src/main/java/com/codecool/firstaidcompetition/exception/StationNotFoundException.java
+++ src/main/java/com/codecool/firstaidcompetition/exception/StationNotFoundException.java
@@ -3,5 +3,9 @@ package com.codecool.firstaidcompetition.exception;
 /**
  * Created by keli on 2017.10.17..
  */
-public class StationNotFoundException {
+public class StationNotFoundException extends RuntimeException {
+
+    public StationNotFoundException(String msg) {
+        super(msg);
+    }
 }
