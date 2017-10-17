cecd57692e851bd79e3f79b95ec7a4273c7c446f
Balázs Máté <balazsm86@gmail.com>
2017-10-17 22:15:58 +0200

    Creating competition services, added rest service for getting competitions

--- src/main/java/com/codecool/firstaidcompetition/repository/ExerciseRepository.java
+++ src/main/java/com/codecool/firstaidcompetition/repository/ExerciseRepository.java
@@ -6,4 +6,3 @@ import org.springframework.data.repository.CrudRepository;
 public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
     Iterable<Exercise> findByStationId(Long stationId);
 }
-
