ed68698aef52624b50bb8f61809b804f4231037c
keligeri <kel.gergo@gmail.com>
2017-10-17 15:18:25 +0200

    Comment custonAuthentication and add in-memory authentication, and add assigned /station/** to ADMIN  role!

--- src/main/java/com/codecool/firstaidcompetition/config/SecurityConfigurator.java
+++ src/main/java/com/codecool/firstaidcompetition/config/SecurityConfigurator.java
@@ -4,57 +4,55 @@ import com.codecool.firstaidcompetition.service.CustomAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by keli on 2017.09.01..
  */
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+//@EnableWebSecurity
+@EnableGlobalMethodSecurity
 public class SecurityConfigurator extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean   // Add encoder as bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    private CustomAuthenticationProvider customAuthenticationProvider;
+    // Comment in temporary
+//    @Autowired
+//    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(customAuthenticationProvider);
+//    }
 
     @Autowired
     public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(customAuthenticationProvider);
+        auth
+            .inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
     }
 
     @Override   // configure our security policy
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-            authorizeRequests()
-                .antMatchers("/admin/**",
-                        "/competition/add", "/registration", "/station/add",
-                        "/exercise/add", "/exercise/delete/**").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("REFEREE")
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-            .and()
-                .formLogin()
-                .loginPage("/login")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .successForwardUrl("/index")
-                .permitAll()
-            .and()
-                .logout()
-                .logoutSuccessUrl("/login?logout")
-                .permitAll()
-            .and()
-                .exceptionHandling()
-                .accessDeniedPage("/index");
+        http.csrf().disable();
+        http.authorizeRequests()
+            .antMatchers("/station/**").hasRole("ADMIN")
+            .anyRequest().permitAll()
+        .and()
+            .httpBasic()
+        .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
