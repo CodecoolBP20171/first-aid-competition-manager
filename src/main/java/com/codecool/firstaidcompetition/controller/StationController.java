96cd2d2ef22f7bc0997eac5a4cf42841887bf6c5
keligeri <kel.gergo@gmail.com>
2017-10-17 15:21:05 +0200

    Add basic route with 4 http verbs and change controller to RestController

--- src/main/java/com/codecool/firstaidcompetition/controller/StationController.java
+++ src/main/java/com/codecool/firstaidcompetition/controller/StationController.java
@@ -1,91 +1,60 @@
 package com.codecool.firstaidcompetition.controller;
 
-import com.codecool.firstaidcompetition.model.Competition;
-import com.codecool.firstaidcompetition.model.Exercise;
 import com.codecool.firstaidcompetition.model.Station;
-import com.codecool.firstaidcompetition.repository.CompetitionRepository;
-import com.codecool.firstaidcompetition.repository.StationRepository;
+import com.codecool.firstaidcompetition.service.StationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
-@Controller
+@RestController
 @RequestMapping("/station")
 public class StationController {
 
     private static final Logger logger = LoggerFactory.getLogger(StationController.class);
 
+    private final StationService stationService;
+
     @Autowired
-    private StationRepository stationRepository;
-    @Autowired
-    private CompetitionRepository competitionRepository;
-
-    @GetMapping(value = {"/" , ""})
-    public String getStations(Model model) {
-        Iterable<Station> stationList = stationRepository.findAll();
-        model.addAttribute("listOfStations", stationList);
-        model.addAttribute("station", new Station());
-        logger.info("Mapping to the station route");
-        return "station/station_table";
+    public StationController(StationService stationService) {
+        this.stationService = stationService;
     }
 
-    @PostMapping(value = {"/" , ""})
-    private ModelAndView saveEditedStation(@ModelAttribute Station station){
-        Station editedStation = stationRepository.findOne(station.getId());
-        editedStation.setName(station.getName());
-        editedStation.setNumber(station.getNumber());
-        editedStation.setDescription(station.getDescription());
-//        editedStation.setCompetition(station.getCompetition());
-
-        stationRepository.save(editedStation);
-        return new ModelAndView("redirect:/station");
+    @GetMapping(value = {"/", ""})
+    public Iterable<Station> getAll() {
+        logger.info("Get all stations from the db");
+        return stationService.findAll();
     }
 
-    @GetMapping("/{competitionId}")
-    private String listByCompetitionId(@PathVariable Long competitionId, Model model){
-        Iterable<Station> stations = stationRepository.findByCompetitionId(competitionId);
-        model.addAttribute("listOfStations", stations);
-        model.addAttribute("station", new Station());
+    @GetMapping(value = "/{id}")
+    public Station getById(@PathVariable long id) {
+        logger.info("Get Station with id {} from the db", id);
 
-        return "station/station_table";
+        return stationService.findById(id);
     }
 
+    @PostMapping(value = {"/", ""})
+    public String save(@RequestBody Station station) {
+        stationService.save(station);
 
-    @GetMapping(value = "/add")
-    public String addStation(Model model) {
-        Iterable<Competition> competitionList = competitionRepository.findAll();
-        model.addAttribute("listOfCompetitions", competitionList);
-        model.addAttribute("station", new Station());
-        return "station/station_form";
+        logger.info("Add Station to the db");
+        return "OK";
     }
 
-    @PostMapping(value = "/add")
-    public ModelAndView submitStation(@ModelAttribute Station station) {
-        stationRepository.save(station);
-        logger.info("Save station to the db, " +
-                        "[name: {}; location: {}; date: {}, owner: {}]",
-                station.getName(), station.getNumber(), station.getDescription(),
-                station.getCompetition());
-        return new ModelAndView("redirect:/station");
+    @PutMapping(value = "/{id}")
+    public String update(@PathVariable long id, @RequestBody Station station) {
+        stationService.update(id, station);
+
+        logger.info("Update Station with {} id in the db", id);
+        return "OK";
     }
 
-    @GetMapping("/delete/{stationId}")
-    private ModelAndView deleteStation(@PathVariable String stationId){
-        stationRepository.delete(Long.valueOf(stationId));
-        logger.info("Deleted thw following station id: {}", stationId);
-        return new ModelAndView("redirect:/station");
-    }
+    @DeleteMapping(value = "/{id}")
+    public String delete(@PathVariable long id) {
+        stationService.delete(id);
 
-    @GetMapping(value = "/edit/{stationId}")
-    @ResponseBody
-    public Station editStation(@PathVariable Long stationId){
-        return stationRepository.findOne(stationId);
+        logger.info("Delete Station with {} id from the db", id);
+        return "OK";
     }
 
 }
