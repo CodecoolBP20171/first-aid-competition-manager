9f8ef250b8974e879a91d83e88658e4257057aea
keligeri <kel.gergo@gmail.com>
2017-10-17 16:15:09 +0200

    Add REsponseEntity as response and exceptionHandling if id is not valid

--- src/main/java/com/codecool/firstaidcompetition/controller/StationController.java
+++ src/main/java/com/codecool/firstaidcompetition/controller/StationController.java
@@ -1,11 +1,17 @@
 package com.codecool.firstaidcompetition.controller;
 
+import com.codecool.firstaidcompetition.exception.StationNotFoundException;
 import com.codecool.firstaidcompetition.model.Station;
 import com.codecool.firstaidcompetition.service.StationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/station")
@@ -27,34 +33,51 @@ public class StationController {
     }
 
     @GetMapping(value = "/{id}")
-    public Station getById(@PathVariable long id) {
-        logger.info("Get Station with id {} from the db", id);
+    public Station getById(@PathVariable long id) throws StationNotFoundException {
+        isValidStationId(id);
+        Station station = stationService.findById(id);
 
-        return stationService.findById(id);
+        logger.info("Get Station with id {} from the db", id);
+        return station;
     }
 
     @PostMapping(value = {"/", ""})
-    public String save(@RequestBody Station station) {
+    public ResponseEntity<String> save(@RequestBody Station station) {
         stationService.save(station);
 
         logger.info("Add Station to the db");
-        return "OK";
+        return new ResponseEntity<>("Created a new station", HttpStatus.OK);
     }
 
     @PutMapping(value = "/{id}")
-    public String update(@PathVariable long id, @RequestBody Station station) {
+    public ResponseEntity<String> update(@PathVariable long id, @RequestBody Station station) throws StationNotFoundException {
+        isValidStationId(id);
         stationService.update(id, station);
 
         logger.info("Update Station with {} id in the db", id);
-        return "OK";
+        return new ResponseEntity<>("Updated existing station", HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{id}")
-    public String delete(@PathVariable long id) {
+    public ResponseEntity<String> delete(@PathVariable long id) throws StationNotFoundException {
+        isValidStationId(id);
         stationService.delete(id);
 
         logger.info("Delete Station with {} id from the db", id);
-        return "OK";
+        return new ResponseEntity<>("Deleted station", HttpStatus.OK);
+    }
+
+    @ExceptionHandler(StationNotFoundException.class)
+    public void handleStationNotFound(StationNotFoundException exception,
+                                      HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    }
+
+    private void isValidStationId(long id) throws StationNotFoundException {
+        Station station = stationService.findById(id);
+        if (station == null) {
+            throw new StationNotFoundException("Station with id: " + id + " not found!");
+        }
     }
 
 }
---
+++ src/main/java/com/codecool/firstaidcompetition/exception/StationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codecool.firstaidcompetition.exception;
+
+/**
+ * Created by keli on 2017.10.17..
+ */
+public class StationNotFoundException {
+}
