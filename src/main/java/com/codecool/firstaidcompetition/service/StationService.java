76352078f62344f36b729100eb05a7b6eaa0690e
keligeri <kel.gergo@gmail.com>
2017-10-17 14:41:20 +0200

    Add stationService code skeleton

---
+++ src/main/java/com/codecool/firstaidcompetition/service/StationService.java
@@ -0,0 +1,43 @@
+package com.codecool.firstaidcompetition.service;
+
+import com.codecool.firstaidcompetition.model.Station;
+import com.codecool.firstaidcompetition.repository.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StationService {
+
+    private final StationRepository stationRepository;
+
+    @Autowired
+    public StationService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    public Iterable<Station> findAll() {
+        return stationRepository.findAll();
+    }
+
+    public Station findById(long id) {
+        return stationRepository.findOne(id);
+    }
+
+    public void save(Station station) {
+        stationRepository.save(station);
+    }
+
+    public void update(long id, Station station) {
+        
+    }
+
+    public void delete(long id) {
+        stationRepository.delete(id);
+    }
+
+    public void deleteAll() {
+        stationRepository.deleteAll();
+    }
+
+
+}
