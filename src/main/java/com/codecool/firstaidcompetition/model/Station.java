06f95ca3e9918a8a004e35a3a57194938cdb77f6
keligeri <kel.gergo@gmail.com>
2017-10-17 15:19:24 +0200

    Add JsonIgnore to id and change JsonIgnore to JsonBackreference on exercises field!

--- src/main/java/com/codecool/firstaidcompetition/model/Station.java
+++ src/main/java/com/codecool/firstaidcompetition/model/Station.java
@@ -1,22 +1,16 @@
 package com.codecool.firstaidcompetition.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@NamedQueries({
-        @NamedQuery(name = "findAllStations", query = "select stat from stations stat"),
-        @NamedQuery(name = "findStationById",
-                query = "select stat from stations stat where stat.id = :stationId"),
-        @NamedQuery(name = "findStationByName",
-                query = "select stat from stations stat where stat.name = :stationName"),
-        @NamedQuery(name = "findStationByNumber",
-                query = "select stat from stations stat where stat.number = :stationNumber"),
-})
 @Entity(name = "stations")
 public class Station {
+
+    @JsonIgnore
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -30,12 +24,11 @@ public class Station {
     @Column(length = 1000)
     private String description;
 
-    @JsonIgnore
     @ManyToOne(cascade = CascadeType.REFRESH)
     @JoinColumn(name = "competition_id")
     private Competition competition;
 
-    @JsonIgnore
+    @JsonBackReference
     @OneToMany(mappedBy = "station", cascade = CascadeType.REMOVE)
     private List<Exercise> exercises = new ArrayList<>();
 
@@ -105,7 +98,7 @@ public class Station {
         this.exercises = exercises;
     }
 
-    public void addExercise(Exercise exercise){
+    public void addExercise(Exercise exercise) {
         exercises.add(exercise);
     }
 }
