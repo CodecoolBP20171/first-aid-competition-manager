7e44542f9032a9a3c2f55087cfc7b23bd574ef78
keligeri <kel.gergo@gmail.com>
2017-10-18 18:44:35 +0200

    Add unit test to service layer, but is this make sense?

--- src/test/com/codecool/firstaidcompetition/service/StationServiceTest.java
+++ src/test/com/codecool/firstaidcompetition/service/StationServiceTest.java
@@ -5,80 +5,88 @@ import com.codecool.firstaidcompetition.model.Exercise;
 import com.codecool.firstaidcompetition.model.Station;
 import com.codecool.firstaidcompetition.repository.CompetitionRepository;
 import com.codecool.firstaidcompetition.repository.StationRepository;
-import org.hibernate.service.spi.InjectService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-public class StationServiceTest {
+import static org.junit.Assert.assertEquals;
 
-    // http://www.baeldung.com/spring-boot-testing
-//    @TestConfiguration
-//    static class StationServiceTestContextConfiguration {
-//        @Bean
-//        StationService stationService() {
-//            return new StationService(Mockito.mock(StationRepository.class),
-//                    Mockito.mock(CompetitionService.class));
-//        }
-//    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class StationServiceTest {
 
     @Autowired
     private StationService stationService;
-    @MockBean
+    @Autowired
     private StationRepository stationRepository;
-    @MockBean
+    @Autowired
     private CompetitionRepository competitionRepository;
-
     private Competition competition;
     private Station newStation1;
     private Station newStation2;
     private Exercise exercise;
 
     @Before
-    public void setup() {
-        updateDb();
-    }
-
     public void updateDb() {
         this.competition = new Competition("Teszt verseny", "Valamilyen hely", "2025-05-25", null);
-        competitionRepository.save(competition);
-        Mockito.when(competitionRepository.findOne(competition.getId()))
+        Mockito.when(competitionRepository.findOne(1L))
                 .thenReturn(competition);
 
-        this.newStation1 = new Station("Teszt 1.0", 44, "Teszt leírás 1.0", competition);
-        this.newStation2 = new Station("Teszt 2.0", 55, "Teszt leírás 2.0", competition);
-        stationRepository.save(newStation1);
-        Mockito.when(stationRepository.findOne(newStation1.getId()))
-                .thenReturn(newStation1);
+        this.newStation1 = new Station("Teszt 1.0", 44
+                , "Teszt leírás 1.0", competition);
+        this.newStation2 = new Station("Teszt 2.0", 55,
+                "Teszt leírás 2.0", competition);
 
+        Mockito.when(stationRepository.findOne(1L)).thenReturn(newStation1);
         this.exercise = new Exercise("Exercise 1.0", "Exszerszájz");
     }
 
     @Test
     public void save_SaveStation_IfAddOneStation() {
-        System.out.println(stationRepository.findOne(1L).getName());
-//        stationService.findAll().forEach(s -> System.out.println(s.getName()));
+        System.out.println(stationService.findById(1L).getName());
 
-        //        assertThat(found.getName()).
-//        stationService.findById(1L).getName());
+        Station station = new Station("Teszt 3.0", 44, "Teszt leírás 3.0",
+                competition);
+        stationService.save(station);
 
-//        Station station = new Station("Teszt 3.0", 44, "Teszt leírás 3.0",
-//                competition, Arrays.asList(exercise));
-//        stationService.save(station);
-//
-//        Station addedStation = stationRepository.findOne(station.getId());
+        Station addedStation = stationRepository.findOne(station.getId());
 //        long size = stationRepository.findAll().spliterator().getExactSizeIfKnown();
-//
+
 //        assertEquals(3, size);
-//        assertEquals(competition.getName(), addedStation.getCompetition().getName());
+        assertEquals(newStation1, addedStation);
 //        assertEquals(station.getDescription(), addedStation.getDescription());
 //        assertEquals(station.getNumber(), addedStation.getNumber());
 //        assertEquals(station.getExercises(), addedStation.getExercises());
+    }
+
+    // https://dzone.com/articles/junit-testing-spring-mvc-0
+    @Configuration
+    static class StationServiceTestContextConfiguration {
+        @Bean
+        StationRepository stationRepository() {
+            return Mockito.mock(StationRepository.class);
+        }
+
+        @Bean
+        CompetitionRepository competitionRepository() {
+            return Mockito.mock(CompetitionRepository.class);
+        }
+
+        @Bean
+        CompetitionService competitionService() {
+            return Mockito.mock(CompetitionService.class);
+        }
+
+        @Bean
+        StationService stationService() {
+            return new StationService(stationRepository(), competitionService());
+        }
     }
 
 }
