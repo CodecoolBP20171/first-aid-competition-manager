0587a61bc4897a700a2219f9276578c67c542f95
keligeri <kel.gergo@gmail.com>
2017-10-19 16:30:55 +0200

    Add stationService testing with some basic example

--- src/test/com/codecool/firstaidcompetition/service/StationServiceTest.java
+++ src/test/com/codecool/firstaidcompetition/service/StationServiceTest.java
@@ -8,85 +8,65 @@ import com.codecool.firstaidcompetition.repository.StationRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+// Helper: https://stackoverflow.com/questions/37727311/how-do-i-junit-test-a-spring-autowired-constructor
+@RunWith(MockitoJUnitRunner.class)
 public class StationServiceTest {
 
-    @Autowired
-    private StationService stationService;
-    @Autowired
-    private StationRepository stationRepository;
-    @Autowired
-    private CompetitionRepository competitionRepository;
-    private Competition competition;
+    @InjectMocks private StationService stationService;
+
+    @Mock private StationRepository stationRepository;
+    @Mock private CompetitionRepository competitionRepository;
+
     private Station newStation1;
     private Station newStation2;
+    private List<Station> stationList;
+
+    private Competition competition;
     private Exercise exercise;
 
     @Before
     public void updateDb() {
-        this.competition = new Competition("Teszt verseny", "Valamilyen hely", "2025-05-25", null);
-        Mockito.when(competitionRepository.findOne(1L))
-                .thenReturn(competition);
+        competition = new Competition("Teszt verseny", "Valamilyen hely", "2025-05-25", null);
 
-        this.newStation1 = new Station("Teszt 1.0", 44
-                , "Teszt leírás 1.0", competition);
-        this.newStation2 = new Station("Teszt 2.0", 55,
+        newStation1 = new Station("Teszt 1.0", 44,
+                "Teszt leírás 1.0", competition);
+        newStation2 = new Station("Teszt 2.0", 55,
                 "Teszt leírás 2.0", competition);
+        stationList = Arrays.asList(newStation1, newStation2);
 
+        Mockito.when(stationRepository.findAll()).thenReturn(stationList);
         Mockito.when(stationRepository.findOne(1L)).thenReturn(newStation1);
-        this.exercise = new Exercise("Exercise 1.0", "Exszerszájz");
+
+        exercise = new Exercise("Exercise 1.0", "Exszerszájz");
     }
 
     @Test
-    public void save_SaveStation_IfAddOneStation() {
-        System.out.println(stationService.findById(1L).getName());
-
-        Station station = new Station("Teszt 3.0", 44, "Teszt leírás 3.0",
-                competition);
-        stationService.save(station);
-
-        Station addedStation = stationRepository.findOne(station.getId());
-//        long size = stationRepository.findAll().spliterator().getExactSizeIfKnown();
-
-//        assertEquals(3, size);
-        assertEquals(newStation1, addedStation);
-//        assertEquals(station.getDescription(), addedStation.getDescription());
-//        assertEquals(station.getNumber(), addedStation.getNumber());
-//        assertEquals(station.getExercises(), addedStation.getExercises());
+    public void findStation_WhenGetById_ThenReturnOneStation() {
+        assertEquals(newStation1, stationService.findById(1L));
     }
 
-    // https://dzone.com/articles/junit-testing-spring-mvc-0
-    @Configuration
-    static class StationServiceTestContextConfiguration {
-        @Bean
-        StationRepository stationRepository() {
-            return Mockito.mock(StationRepository.class);
-        }
-
-        @Bean
-        CompetitionRepository competitionRepository() {
-            return Mockito.mock(CompetitionRepository.class);
-        }
-
-        @Bean
-        CompetitionService competitionService() {
-            return Mockito.mock(CompetitionService.class);
-        }
-
-        @Bean
-        StationService stationService() {
-            return new StationService(stationRepository(), competitionService());
-        }
+    @Test
+    public void findStations_WhenFindAll_ThenReturnWithList() {
+        assertEquals(stationList, stationService.findAll());
     }
 
 }
