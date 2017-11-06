d1cefbef843de8a26f9f84e666677354740f28bd
keligeri <kel.gergo@gmail.com>
2017-11-06 13:04:03 +0100

    Create new exception for Exercise entity

---
+++ src/main/java/com/codecool/firstaidcompetition/exception/ExerciseNotFoundException.java
@@ -0,0 +1,11 @@
+package com.codecool.firstaidcompetition.exception;
+
+/**
+ * Created by keli on 2017.11.06..
+ */
+public class ExerciseNotFoundException extends RuntimeException {
+
+    public ExerciseNotFoundException(String msg) {
+        super(msg);
+    }
+}
